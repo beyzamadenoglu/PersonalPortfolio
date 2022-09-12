57bc03fdc15b90b1c25a9bbea2932a793740d8f3
Beyza Madenoglu <beyza_maden97@hotmail.com>
2022-09-12 18:06:47 +0300

    mobile styling

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,7 +6,7 @@ const Content = () => {
     return (
         <div className="container-lg content">
             <div className="row">
-                <div className="col-6 align-bottom"><h2 className="about-text">Frontend Developer and  QA Engineer.</h2></div>
+                <div className="col-6 align-bottom"><h1 className="about-text">Frontend Developer and  QA Engineer.</h1></div>
                 <div className="col-12 col-md-6 container" id="blob-container">
                     <Blob />
                     <img id="profile-picture" src={Profile} alt="pff"></img>
