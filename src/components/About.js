8322bf9fae922340119b471df44a4518268f75b4
Beyza Madenoğlu <beyza_maden97@hotmail.com>
2022-12-16 12:50:12 +0300

    updates and social style change

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div id="about-me" className="container-fluid about">
-            <div className="row">
-                <div className="col-12 h-about"><p>
-                    <h2>About me</h2>
-                </p></div>
-                <div className="col-12">
-                    <p className="p-about">
-                        Hi, it's me Beyza Madenoğlu. I graduated from Istanbul Aydin University Computer Engineering Department in June 2021. I have been working as a Software QA Engineer in a squad that provides Call Center Solutions at VodafoneTR for more than a year. During this time, I focused on the Frontend field and started to develop myself. I have developed various projects with React in the frontend area. (Like this page &#128579;.) I also attended a React Bootcamp, which I believe has been very beneficial for me. The next thing that will take me one step further will be to start a professional career as a Frontend Developer.
-                    </p>
-                </div>
-            </div>
-
+  return (
+    <div id="about-me" className="container-fluid about">
+      <div className="row">
+        <div className="col-12 h-about">
+          <p>
+            <h2>About me</h2>
+          </p>
         </div>
+        <div className="col-12">
+          <p className="p-about">
+            Hi, it's me Beyza Madenoğlu. I graduated from Istanbul Aydin
+            University Computer Engineering Department in June 2021. I had been
+            working as a Software QA Engineer in a squad which provides Call
+            Center Solutions at Vodafone for more than a year, now I am working
+            as a Frontend Developer. I develop apps with JavaScript,
+            React and Vue.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default About
+export default About;
--- src/components/Social.js
+++ src/components/Social.js
@@ -14,12 +14,12 @@ const Social = () => {
         <h2>Social Accounts</h2>
       </div>
       <div className="row">
-        <a className="col-lg-3 col-sm-12 mb-4 mb-md-0" href="https://www.linkedin.com/in/beyzamadenoglu/" target="_blank"><AiOutlineLinkedin size={40} color={"#4b4843"} />  </a>
-        <a className="col-lg-3 col-sm-12 mb-4 mb-md-0" href="https://twitter.com/beyzamadenoglu" target="_blank"><FiTwitter size={40} color={"#4b4843"} /></a>
-        <a className="col-lg-3 col-sm-12 mb-4 mb-md-0" href="https://github.com/beyzamadenoglu" target="_blank"><FiGithub size={40} color={"#4b4843"} /></a>
-        <a className="col-lg-3 col-sm-12 mb-4 mb-md-0" href="https://medium.com/@beyzamadenoglu" target="_blank"><AiOutlineMedium size={40} color={"#4b4843"} /></a>
-      </div>
+        <a className="col-lg-2 col-sm-12 mb-4 mb-md-0" href="https://www.linkedin.com/in/beyzamadenoglu/" target="_blank"><AiOutlineLinkedin size={40} color={"#4b4843"} />  </a>
+        <a className="col-lg-2 col-sm-12 mb-4 mb-md-0" href="https://twitter.com/beyzamadenoglu" target="_blank"><FiTwitter size={40} color={"#4b4843"} /></a>
+        <a className="col-lg-2 col-sm-12 mb-4 mb-md-0" href="https://github.com/beyzamadenoglu" target="_blank"><FiGithub size={40} color={"#4b4843"} /></a>
+        <a className="col-lg-2 col-sm-12 mb-4 mb-md-0" href="https://medium.com/@beyzamadenoglu" target="_blank"><AiOutlineMedium size={40} color={"#4b4843"} /></a>
     </div>
+  </div>
   )
 }
 
