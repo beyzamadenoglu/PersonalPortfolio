99e6e6a85dfe47901ec8708d79431de6c75e3191
Beyza Madenoglu <beyza_maden97@hotmail.com>
2024-02-05 17:04:14 +0300

    update medium image

--- public/env.js
+++ public/env.js
@@ -1,6 +1,1 @@
-window.env = {
-  "SERVICE_ID": "service_2ypeadr",
-  "PUBLIC_KEY": "8ZKWaJAWXuVCO6hTR",
-  "PRIVATE_KEY": "thfGT_9YoLKwSCzkIcMwh",
-  "TEMPLATE_ID": "template_khh5d78"
-};
+window.env = {};
--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -3,14 +3,16 @@ import ToText from '../Utils/ToText';
 
 function ArticleCard(props) {
 
+    console.log(props)
     function OpenArticle(url) {
         window.open(url, '_blank', 'noopener,noreferrer');
+
     }
     return (
         <div className="col-12 col-md-3 medium-container">
             <div className="medium-card" onClick={() => OpenArticle(props.link)}>
                 <div className="medium-img">
-                    <img src={props.image} alt="medium-img">
+                    <img src="https://miro.medium.com/v2/resize:fit:1400/1*jfdwtvU6V6g99q3G7gq7dQ.png" alt="medium-img">
                     </img>
                 </div>
                 <div>
